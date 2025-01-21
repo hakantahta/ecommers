5e23426fe74051388c3d3ab551cd25041645dd20
Hakan TAHTA <126967585+hakantahta@users.noreply.github.com>
2025-01-21 17:59:58 +0300

    edit CategoryController add categoryresponsedto

--- src/main/java/com/ecommerce/controller/CategoryController.java
+++ src/main/java/com/ecommerce/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.dto.request.CategoryRequestDto;
+import com.ecommerce.dto.response.CategoryResponseDto;
 import com.ecommerce.model.Category;
 import com.ecommerce.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,28 +18,51 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<Category>> getAllCategories() {
-        return ResponseEntity.ok(categoryService.getAllCategories());
+    public ResponseEntity<List<CategoryResponseDto>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        // DTO dönüşümü yapılabilir
+        return ResponseEntity.ok(categories.stream().map(this::convertToResponseDto).toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
-        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    public ResponseEntity<CategoryResponseDto> getCategoryById(@PathVariable Long id) {
+        Category category = categoryService.getCategoryById(id);
+        return ResponseEntity.ok(convertToResponseDto(category));
     }
 
     @PostMapping
-    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
-        return ResponseEntity.ok(categoryService.createCategory(category));
+    public ResponseEntity<CategoryResponseDto> createCategory(@RequestBody CategoryRequestDto categoryRequestDto) {
+        Category category = new Category();
+        category.setName(categoryRequestDto.getName());
+        category.setType(categoryRequestDto.getType());
+        category.setParentCategoryId(categoryRequestDto.getParentCategoryId());
+        Category createdCategory = categoryService.createCategory(category);
+        return ResponseEntity.ok(convertToResponseDto(createdCategory));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category category) {
-        return ResponseEntity.ok(categoryService.updateCategory(id, category));
+    public ResponseEntity<CategoryResponseDto> updateCategory(@PathVariable Long id, @RequestBody CategoryRequestDto categoryRequestDto) {
+        Category category = new Category();
+        category.setId(id);
+        category.setName(categoryRequestDto.getName());
+        category.setType(categoryRequestDto.getType());
+        category.setParentCategoryId(categoryRequestDto.getParentCategoryId());
+        Category updatedCategory = categoryService.updateCategory(id, category);
+        return ResponseEntity.ok(convertToResponseDto(updatedCategory));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
         categoryService.deleteCategory(id);
         return ResponseEntity.noContent().build();
+    }
+
+    private CategoryResponseDto convertToResponseDto(Category category) {
+        CategoryResponseDto dto = new CategoryResponseDto();
+        dto.setId(category.getId());
+        dto.setName(category.getName());
+        dto.setType(category.getType());
+        dto.setParentCategoryId(category.getParentCategoryId());
+        return dto;
     }
 } 
